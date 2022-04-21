75512eb0b2e144abf7120101d4afec462b5e32a5
victor <victor.morales@tieto.com>
2022-04-21 09:41:43 +0300

    hard code tgw data resource

--- klz_type1/terraform/tgw-quickndirty.tf
+++ klz_type1/terraform/tgw-quickndirty.tf
@@ -1,3 +1,4 @@
 data "aws_ec2_transit_gateway" "tgw" {
-    id = data.terraform_remote_state.networking.outputs.ss_infrastructure_tgw_ids[*].ireland_tgw
+    # id = data.terraform_remote_state.networking.outputs.ss_infrastructure_tgw_ids[*].ireland_tgw
+    id = "tgw-085f17a478f26a2d2"
 }
