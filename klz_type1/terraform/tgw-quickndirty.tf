85ea478b19e25982ad980b3723226d77756cb876
victor <victor.morales@tieto.com>
2022-04-21 09:24:06 +0300

    gix tgw data resource

--- klz_type1/terraform/tgw-quickndirty.tf
+++ klz_type1/terraform/tgw-quickndirty.tf
@@ -1,3 +1,3 @@
 data "aws_ec2_transit_gateway" "tgw" {
-    id = data.terraform_remote_state.networking.outputs.ss_infrastructure_tgw_ids[ireland_tgw]
+    id = data.terraform_remote_state.networking.outputs.ss_infrastructure_tgw_ids[*].ireland_tgw
 }
