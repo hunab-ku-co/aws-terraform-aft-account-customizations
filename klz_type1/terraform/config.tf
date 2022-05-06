10d28213730b7d8613e18fef49a34b21d01560d9
victor <victor.morales@tieto.com>
2022-05-06 09:21:15 +0300

    add empty customization, delete unnecessary lines from config, delete network nic

--- klz_type1/terraform/config.tf
+++ klz_type1/terraform/config.tf
@@ -29,28 +29,16 @@ data "utils_deep_merge_json" "config" {
 #
 
 locals {
-  # Customer Azure Subscrption information
+  # Customer AWS Subscrption information
   customer = lookup(local.config, "customer", {})
   # Common defaults for SAP Landscape-wide resources
   default = lookup(local.config, "default", {})
-  # Azure Resource abpreviations
+  # AWS Resource abpreviations
   resource_name = lookup(local.config, "resource_name", {})
-  # Naming Scheme for Azure Resources
+  # Naming Scheme for AWS Resources
   customer_naming_scheme = lookup(local.config, "customer_naming_scheme", {})
-  # Common tags for the environment and Azure Resources
+  # Common tags for the environment and AWS Resources
   tags = lookup(local.config, "tags", {})
-  # Diagnostics settings
-  diagnostics = lookup(local.config, "diagnostics", {})
-  # Diagnostics enabled for Azure Resources
-  diagnostics_enabled = lookup(local.diagnostics, "enabled", {})
-  # Diagnostics logging categories for Azure Resources
-  diagnostics_categories = lookup(local.diagnostics, "categories", {})
-  # Management Locks for Azure Resources
-  management_lock = lookup(local.config, "management_lock", {})
   # Group map deployment to Resources Groups and default values
   group = lookup(local.config, "groups", {})
-  # Azure Resource Groups
-  resource_group = lookup(local.config, "resource_groups", {})
-  # Azure Log Analytics
-  config_log_analytics = lookup(local.config, "log_analytics", {})
 }
